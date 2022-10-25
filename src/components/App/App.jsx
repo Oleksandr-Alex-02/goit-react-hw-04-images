752bba76d32033e0fae0cdae98c35b271b92b4d6
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-25 21:36:09 +0300

    --

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import Searchbar from '../Searchbar/Searchbar';
-import ImageGallery from '../ImageGallery/ImageGallery';
-import Modal from '../Modal/Modal'
+// import ImageGallery from '../ImageGallery/ImageGallery';
+// import Modal from '../Modal/Modal'
 import css from './App.module.css'
 
+// const axios = require('axios').default;
+
 class App extends Component {
+
+  // async componentDidMount() {
+  //   await fetch('https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12')
+  //     .then(res => res.json())
+  //     .then(console.log)
+  // }
+
+
   render() {
     return (
       <div className={css.App}>
         <Searchbar />
-        <ImageGallery />
-        <Modal />
+        {/* <ImageGallery /> */}
+        {/* <Modal /> */}
       </div>
     )
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,18 +2,34 @@ import React, { Component } from 'react'
 import css from './Searchbar.module.css'
 
 class Searchbar extends Component {
+    state = {
+        searchName: '',
+    };
+    handleNameChange = e => {
+        this.setState({ searchName: e.currentTarget.value.toLowerCase() });
+    };
+    handleSearch = e => {
+        e.preventDefault();
+    };
+
+
+
     render() {
         return (
             <header className={css.searchbar}>
                 <form className={css.searchbar__form}>
-                    <button type="submit" className={css.searchbar__submit}>
+                    <button className={css.searchbar__submit} type="submit">
                         <span className="button-label">Search</span>
                     </button>
 
                     <input
                         className={css.searchbar__input}
                         type="text"
+                        autoComplete="off"
+                        autoFocus
                         placeholder="Search images and photos"
+                        value={this.state.searchName}
+                        onChange={this.handleNameChange}
                     />
                 </form>
             </header>
